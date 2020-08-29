b39520950efed45700abfa9070ff23b465a16642
Seungyun Lee <lsy0914@naver.com>
2020-08-29 18:51:55 +0900

    Immer 실습

--- tutorial_src/src/App.js
+++ tutorial_src/src/App.js
@@ -1,4 +1,5 @@
 import React, { useRef, useReducer, useMemo, useCallback, createContext } from 'react';
+import produce from 'immer';
 import './App.css';
 import Counter from './Counter';
 import InputSample from './inputSample';
@@ -37,24 +38,19 @@ function countActiveUsers(users) {
 function reducer(state, action) {
   switch (action.type) {
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(
-          user => user.id === action.id
-          ? { ...user, active: !user.active }
-          : user
-          )
-      };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      }
+      return produce(state, draft => {
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       throw new Error('Unhandled aciton type');
   }
